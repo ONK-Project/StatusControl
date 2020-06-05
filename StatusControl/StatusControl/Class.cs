15460d66008832b56b81ff92117fe8b5d0392973
jonas.hingebjerg@gmail.com <jonas.hingebjerg@gmail.com>
2020-06-05 10:54:53 +0200

    asda

---
+++ StatusControl/StatusControl/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatusControl
+{
+    public class Class
+    {
+    }
+}
---
+++ StatusControl/StatusControl/Controllers/StatusControlController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StatusControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusControlController : ControllerBase
+    {
+       
+
+        private readonly ILogger<StatusControlController> _logger;
+
+        public StatusControlController(ILogger<StatusControlController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            
+        }
+    }
+}
